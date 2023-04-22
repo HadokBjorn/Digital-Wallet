3e79207b179a0e6e5c5dc8495a7ddb30f23dbe76
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-04-22 04:44:26 -0500

    Post /Cadastro basic function done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import express, { json } from 'express';
-import { MongoClient, ObjectId } from 'mongodb';
-import { stripHtml } from 'string-strip-html';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import joi from 'joi';
+import cors from "cors";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import express, { json } from "express";
+import joi from "joi";
+import { MongoClient, ObjectId } from "mongodb";
+import { stripHtml } from "string-strip-html";
 
 const app = express();
 const PORT = 5000;
@@ -12,7 +13,6 @@ app.use(json());
 app.use(cors());
 dotenv.config();
 
-// eslint-disable-next-line no-undef
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
@@ -21,6 +21,22 @@ try {
 	print(err.message);
 }
 const db = mongoClient.db();
+
+//Estrutura de cadastro {name, email, password}
+
+app.post("/cadastro", async (req, res) => {
+	const { name, email, password } = req.body;
+	try {
+		const findUser = await db.collection("cadastro").findOne({ email });
+		if (findUser) return res.status(409).send("E-mail já cadastrado!");
+		const hash = bcrypt.hashSync(password, 10);
+		await db.collection("usuario").insertOne({ name, email, password: hash });
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
+//Estrutura de Login {email e password}
 
 //Blocos de código do projeto
 
